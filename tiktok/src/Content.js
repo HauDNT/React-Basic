3d50f4b10de1685a6627668c5c4c8b2f2cee8025
HauDNT <tienhau.it@gmail.com>
2024-07-12 16:14:11 +0700

    useEffect scroll event, cleanup funtion & memory leak

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users'];
+const tabs = ["posts", "comments", "albums", "photos", "todos", "users"];
 
 function Content() {
     // 1. useEffect(callback)
@@ -13,44 +13,70 @@ function Content() {
 
     // --------
     // 1. Callback luôn được gọi sau khi component mounted
-    // 2. 
+    // 2. Cleanup function luôn được gọi trước khi component unmounted
 
-    const [title, setTitle] = useState('');
-    const [type, setType] = useState('posts');
+    const [title, setTitle] = useState("");
+    const [type, setType] = useState("posts");
     const [data, setData] = useState([]);
+    const [showGoToTop, setShowGoToTop] = useState(false);
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res => res.json())
-        .then(newData => {
+        .then((res) => res.json())
+        .then((newData) => {
             setData(newData);
-        })
+        });
     }, [type]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        console.log(window.scrollY);
+
+        if (window.scrollY) {
+            setShowGoToTop(true);
+        } else {
+            setShowGoToTop(false);
+        }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        
+        // Clenup function (dọn dẹp bộ nhớ)
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
 
     return (
         <>
-            {
-                tabs.map(tab => (
-                    <button
-                        onClick={() => setType(tab)}
-                        key={tab}>
-                            {tab}
-                    </button>
-                ))
-            }
+            {tabs.map((tab) => (
+                <button onClick={() => setType(tab)} key={tab}>
+                {tab}
+                </button>
+            ))}
 
-            <input
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
-            
+            <input value={title} onChange={(e) => setTitle(e.target.value)} />
+
             <ul>
-                {data && data.map(item => (
-                    <li key={item.id}>{item.title ?? item.name}</li>
-                ))}
+                {data &&
+                data.map((item) => <li key={item.id}>{item.title ?? item.name}</li>)}
             </ul>
+
+            {
+                showGoToTop && (
+                    <button
+                        style={{
+                            position: 'fixed',
+                            right: 20,
+                            bottom: 20
+                        }}
+                    >
+                        Go to top
+                    </button>
+                )
+            }
         </>
-    )
+    );
 }
 
 export default Content;
