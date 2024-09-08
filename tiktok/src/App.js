d7ec2f955ed01553e3a6ce2702b40f6e5945fc9a
Thomas <tienhau.it@gmail.com>
2024-09-08 19:33:55 +0700

    Video 45: useReducer() to do app

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,17 +1,111 @@
-import { useState } from "react";
-import Content from "./Content";
+import React, { useReducer, useRef } from "react";
+
+const initState = {
+    job: '',
+    jobs: [],
+};
+
+const SET_JOB = 'set_job';
+const ADD_JOB = 'add_job';
+const DELETE_JOB = 'delete_job';
+
+const setJob = (payload) => {
+    return {
+        type: SET_JOB,
+        payload
+    };
+};
+
+const addJob = (payload) => {
+    return {
+        type: ADD_JOB,
+        payload
+    };
+};
+
+const deleteJob = (payload) => {
+    return {
+        type: DELETE_JOB,
+        payload
+    };
+};
+
+const reducer = (state, action) => {
+    let newState;
+
+    switch (action.type) {
+        case SET_JOB:
+            newState = {
+                ...state,
+                job: action.payload,
+            };
+
+            break;
+        case ADD_JOB: 
+            newState = {
+                ...state,
+                jobs: [...state.jobs, action.payload]
+            };
+            break;
+        case DELETE_JOB: 
+            const newJobs = [...state.jobs];
+            newJobs.splice(action.payload, 1);
+
+            newState = {
+                ...state,
+                jobs: newJobs,
+            };  
+            break;
+        default:
+            throw new Error("Invalid action");
+    };
+
+    return newState;
+};
 
 function App() {
-    const [show, setShow] = useState(false);
+    const [state, dispatch] = useReducer(reducer, initState);
+    const { job, jobs } = state;
+    const inputRef = useRef();
+
+    const handleSubmit = () => {
+        dispatch(addJob(job));
+        dispatch(setJob(''));
+
+        inputRef.current.focus();
+    };
 
     return (
-        <>
-            <button onClick={() => setShow(!show)}>Toggle</button>
-            {
-                show && <Content/>
-            }
-        </>
-    );
+        <div>
+            <h3>Todo</h3>
+            <input 
+                ref={inputRef}
+                type="text" 
+                value={job}
+                onChange={(e) => {
+                    dispatch(setJob(e.target.value))
+                }}
+                placeholder="Enter todo..." 
+            />
+            <button onClick={handleSubmit}>Add</button>
+
+            <ul>
+                {
+                    jobs.map((job, index) => (
+                        <li key={index}>
+                            {job}
+                            <span 
+                                onClick={() => dispatch(deleteJob(index))}
+                                style={{marginLeft: 30}}
+                            >
+                                x
+                            </span>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default App;
--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,41 +1,15 @@
-import { useEffect, useState } from "react";
+import { memo } from "react";
 
-const tabs = ["posts", "comments", "albums", "photos", "todos", "users"];
-
-function Content() {
-    // 1. useEffect(callback)
-    //  - Gọi callback mỗi khi component re-render
-    //  - Gọi callback sau khi component thêm element vào DOM.
-    // 2. useEffect(callback, [])
-    //  - Chỉ gọi callback 1 lần sau khi component mounted.
-    // 3. useEffect(callback, [dependencies])
-    //  - Chỉ gọi lại khi dependencies thay đổi.
-
-    // --------
-    // 1. Callback luôn được gọi sau khi component mounted
-    // 2. Cleanup function luôn được gọi trước khi component unmounted
-
-    const [width, setWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        // Cleanup function:
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
+function Content({onIncrease}) {
+    console.log('Re-render');
+    
 
     return (
         <>
-            <h1>{width}</h1>
+            <h2>Hellow</h2>
+            <button onClick={onIncrease}>Click me!</button>
         </>
-    );
+    )
 }
 
-export default Content;
+export default memo(Content);
