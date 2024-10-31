39235d3acf4a0d2ca1439299de637f312c41f945
Thomas <tienhau.it@gmail.com>
2024-10-31 08:16:39 +0700

    Rename tiktok old -> F8-Practice & init new Tiktok project

--- F8-Practice/src/components/Button/Button.js
+++ F8-Practice/src/components/Button/Button.js
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import styles from './Button.module.css'
+import styles from './Button.module.scss'
 
 function Button({ primary, danger }) {
     const classes = clsx(styles.myButton, {
---
+++ tiktok/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            My React App
+        </>
+    )
+}
+
+export default App
